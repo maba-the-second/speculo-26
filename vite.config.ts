33deecae5474c5c1dcae94d5c439601b1b51d3e5
indikarathninda-cyber <nadil.kalpana@gmail.com>
2026-09-01 17:34:49 +0530

    Configure base url and CNAME for custom domain rajans.photography

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/speculo-26/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
